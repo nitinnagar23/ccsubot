b29fc7b7bb0f9159c174eadc80adf8119192f8c0
nitinnagar23 <nitinnagar43@gmail.com>
2025-08-04 13:11:42 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", "123456"))
+API_HASH = os.getenv("API_HASH", "your_api_hash")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+
+OWNER_ID = [int(x) for x in os.getenv("OWNER_ID", "123456789").split()]
+SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "your_support_chat")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "YourBotUsername")
+
+COMMAND_PREFIXES = ["/", "!"]
