50e42119b6597cdfcf57888b4e83e71555b92a44
nitinnagar23 <nitinnagar43@gmail.com>
2025-08-04 13:48:11 +0530

    Create replit.nix

---
+++ replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+  deps = [
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.setuptools
+    pkgs.python311Packages.wheel
+    pkgs.openssl
+    pkgs.git
+  ];
+}
