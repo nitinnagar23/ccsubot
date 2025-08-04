b18a361969977d2ac6a5888ae67399eaf06ae91f
nitinnagar23 <nitinnagar43@gmail.com>
2025-08-04 14:49:12 +0530

    Create help.py

---
+++ handlers/help.py
@@ -0,0 +1,9 @@
+# bot/handlers/help.py
+
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from bot.utils.pagination import paginate_help
+
+@Client.on_message(filters.command("help"))
+async def help_command(client: Client, message: Message):
+    await paginate_help(client, message)
