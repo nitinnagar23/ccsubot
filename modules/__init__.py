6f475e871aaf7c6f59946283ca9369c550a41c65
nitinnagar23 <nitinnagar43@gmail.com>
2025-08-04 14:51:36 +0530

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,23 @@
+# bot/modules/__init__.py
+
+from . import (
+    admin,
+    filters,
+    notes,
+    purges,
+    warnings,
+    welcome,
+    reports,
+    nightmode,
+    rules,
+    misban,
+    misc,
+    games,
+    echo,
+    cleanservice,
+    topics,
+    repeats,
+    globalchat,
+    fsub,
+    help_menu,
+)
